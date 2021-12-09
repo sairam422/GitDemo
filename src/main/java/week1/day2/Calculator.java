f765c950a780a8d11e664af0384fcf1ced04e4e5
Sai Ram <sairam.rain422@gmail.com>
2021-12-09 23:45:50 +0530

    this is commit from asia

--- src/main/java/week1/day2/Calculator.java
+++ src/main/java/week1/day2/Calculator.java
@@ -3,16 +3,20 @@ package week1.day2;
 public class Calculator {
 	
 	public int add(int num1, int num2, int num3) {
+		System.out.println("Adding three numbers"); //Git demo line
 		return num1+num2+num3;
 	}
 	
 	public int sub(int num1, int num2) {
+		System.out.println("Subtracting two numbers"); //Git demo line
 		return num1-num2;
 	}
 	public double mul(double num1, double num2) {
+		System.out.println("Multiplying two numbers"); //Git demo line
 	return num1*num2;
 	}
 	public float divide(float num1, float num2) {
+		System.out.println("Dividing two numbers"); //Git demo line
 	return num1/num2;
 	}
 
